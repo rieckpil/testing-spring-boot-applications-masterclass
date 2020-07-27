9fe91f36bbd41f890328c57ac0c3b25e28b3889d
rieckpil <mail@philipriecks.de>
2020-07-27 09:43:09 +0200

    update dependencies and agenda

---
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
@@ -0,0 +1,24 @@
+package de.rieckpil.courses.book.management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookSynchronizationListenerTest {
+
+  @Test
+  public void shouldRejectBookWhenIsbnIsMalformed() {
+  }
+
+  @Test
+  public void shouldNotOverrideWhenBookAlreadyExists() {}
+
+  @Test
+  public void shouldThrowExceptionWhenProcessingFails() {}
+
+  @Test
+  public void shouldStorBookWhenNewAndCorrectIsbn() {
+
+  }
+
+}
