7a7942e6873fccd036410bced37f6d602ccc5930
rieckpil <mail@philipriecks.de>
2020-08-02 15:13:39 +0200

    next steps JUnit 5

--- src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -1,9 +1,18 @@
 package de.rieckpil.courses.book.review;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static de.rieckpil.courses.book.review.RandomReviewParameterResolverExtension.RandomReview;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(RandomReviewParameterResolverExtension.class)
 class ReviewVerifierTest {
 
   private ReviewVerifier reviewVerifier;
@@ -20,6 +29,42 @@ class ReviewVerifierTest {
 
     boolean result = reviewVerifier.doesMeetQualityStandards(review);
     assertFalse(result, "ReviewVerifier did not detect swear word");
+  }
+
+  @Test
+  @DisplayName("Should fail when review contains 'lorem ipsum'")
+  void testLoremIpsum() {
+
+    String review = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr," +
+      " sed diam nonumy eirmod tempor invidunt ut labore et " +
+      "dolore magna aliquyam erat, sed diam voluptua. " +
+      "At vero eos et accusam et justo duo dolores et ea rebum";
+
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertFalse(result, "ReviewVerifier did not detect lorem ipsum");
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "/badReview.csv")
+  void shouldFailWhenReviewIsOfBadQuality(String review) {
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertFalse(result, "ReviewVerifier did not detect bad review");
+  }
+
+  @RepeatedTest(5)
+  void shouldFailWhenRandomReviewQualityIsBad(@RandomReview String review) {
+    System.out.println(review);
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertFalse(result, "ReviewVerifier did not detect random bad review");
+  }
+
+  @Test
+  void shouldPassWhenReviewIsGood() {
+    String review = "I can totally recommend this book " +
+      "who is interested in learning how to write Java code!";
+
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertTrue(result, "ReviewVerifier did not pass a good review");
   }
 
 }
