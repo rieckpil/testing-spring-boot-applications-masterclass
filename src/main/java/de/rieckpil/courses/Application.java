1f09fd09609e95903ad50f35b69169ce88d8cac9
rieckpil <philip.riecks@scout24.com>
2020-08-19 09:24:34 +0200

    ensure Java 14 preview features are usable

--- src/main/java/de/rieckpil/courses/Application.java
+++ src/main/java/de/rieckpil/courses/Application.java
@@ -1,13 +1,28 @@
 package de.rieckpil.courses;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    String welcomeMessage = """
+      \n
+      Welcome to the Testing Spring Boot Applications Masterclass!
+      If you can see this in the console, you successfully started the course application.
+      """;
+
+    LOG.info(welcomeMessage);
+  }
 }
