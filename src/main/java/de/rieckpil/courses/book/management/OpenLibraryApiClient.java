3ef4dc97cd0e65bd9a612ca227e9fc21a72fe919
rieckpil <mail@philipriecks.de>
2020-08-27 07:10:20 +0200

    testing the usage of WebClient

--- src/main/java/de/rieckpil/courses/book/management/OpenLibraryApiClient.java
+++ src/main/java/de/rieckpil/courses/book/management/OpenLibraryApiClient.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 public class OpenLibraryApiClient {
@@ -23,6 +26,7 @@ public class OpenLibraryApiClient {
         .build())
       .retrieve()
       .bodyToMono(ObjectNode.class)
+      .retryWhen(Retry.fixedDelay(2, Duration.ofMillis(200)))
       .block();
 
     JsonNode content = result.get("ISBN:" + isbn);
