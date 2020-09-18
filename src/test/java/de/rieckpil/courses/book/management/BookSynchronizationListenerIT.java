8bbdf706e0eea9180dc3988af313bea1a7247e6d
rieckpil <mail@philipriecks.de>
2020-09-18 08:17:27 +0200

    setup for writing integration tests

---
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerIT.java
@@ -0,0 +1,134 @@
+package de.rieckpil.courses.book.management;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import de.rieckpil.courses.initializer.RSAKeyGenerator;
+import de.rieckpil.courses.initializer.WireMockInitializer;
+import de.rieckpil.courses.stubs.OAuth2Stubs;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
+
+@ActiveProfiles("integration-test")
+@ContextConfiguration(initializers = WireMockInitializer.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BookSynchronizationListenerIT {
+
+  @Container
+  static PostgreSQLContainer database = (PostgreSQLContainer) new PostgreSQLContainer("postgres:12")
+    .withDatabaseName("test")
+    .withUsername("duke")
+    .withPassword("s3cret")
+    .withReuse(true);
+
+  @Container
+  static LocalStackContainer localStack = new LocalStackContainer("0.10.0")
+    .withServices(SQS)
+    .withEnv("DEFAULT_REGION", "eu-central-1")
+    .withReuse(true);
+
+  private static final String QUEUE_NAME = UUID.randomUUID().toString();
+
+  @DynamicPropertySource
+  static void properties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", database::getJdbcUrl);
+    registry.add("spring.datasource.password", database::getPassword);
+    registry.add("spring.datasource.username", database::getUsername);
+    registry.add("sqs.book-synchronization-queue", () -> QUEUE_NAME);
+  }
+
+  @TestConfiguration
+  static class TestConfig {
+    @Bean
+    public AmazonSQSAsync amazonSQSAsync() {
+      return AmazonSQSAsyncClientBuilder.standard()
+        .withCredentials(localStack.getDefaultCredentialsProvider())
+        .withEndpointConfiguration(localStack.getEndpointConfiguration(SQS))
+        .build();
+    }
+  }
+
+  @BeforeAll
+  static void beforeAll() throws IOException, InterruptedException {
+    localStack.execInContainer("awslocal", "sqs", "create-queue", "--queue-name", QUEUE_NAME);
+  }
+
+  static {
+    database.start();
+    localStack.start();
+  }
+
+  @Autowired
+  private QueueMessagingTemplate queueMessagingTemplate;
+
+  @Autowired
+  private WebTestClient webTestClient;
+
+  @Autowired
+  private RSAKeyGenerator rsaKeyGenerator;
+
+  @Autowired
+  private OAuth2Stubs oAuth2Stubs;
+
+  @Test
+  public void shouldGetSuccessWhenClientIsAuthenticated() throws JOSEException {
+
+    JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS256)
+      .type(JOSEObjectType.JWT)
+      .keyID(RSAKeyGenerator.KEY_ID)
+      .build();
+
+    JWTClaimsSet payload = new JWTClaimsSet.Builder()
+      .issuer(oAuth2Stubs.getIssuerUri())
+      .audience("account")
+      .subject("duke")
+      .claim("preferred_username", "duke")
+      .claim("email", "duke@spring.io")
+      .claim("scope", "openid email profile")
+      .claim("azp", "react-client")
+      .claim("realm_access", Map.of("roles", List.of()))
+      .expirationTime(Date.from(Instant.now().plusSeconds(120)))
+      .issueTime(new Date())
+      .build();
+
+    SignedJWT signedJWT = new SignedJWT(header, payload);
+    signedJWT.sign(new RSASSASigner(rsaKeyGenerator.getPrivateKey()));
+
+    this.webTestClient
+      .get()
+      .uri("/api/books/reviews/statistics")
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + signedJWT.serialize())
+      .exchange()
+      .expectStatus().is2xxSuccessful();
+  }
+
+}
---
+++ src/test/java/de/rieckpil/courses/initializer/WireMockInitializer.java
@@ -0,0 +1,49 @@
+package de.rieckpil.courses.initializer;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import de.rieckpil.courses.stubs.OAuth2Stubs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.ContextClosedEvent;
+
+public class WireMockInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+  private static final Logger LOG = LoggerFactory.getLogger(WireMockInitializer.class);
+
+  @Override
+  public void initialize(ConfigurableApplicationContext applicationContext) {
+
+    LOG.info("About to start the WireMockServer");
+
+    WireMockServer wireMockServer = new WireMockServer(new WireMockConfiguration().dynamicPort());
+    wireMockServer.start();
+
+    LOG.info("WireMockServer successfully started");
+
+    RSAKeyGenerator rsaKeyGenerator = new RSAKeyGenerator();
+    rsaKeyGenerator.initializeKeys();
+
+    OAuth2Stubs oAuth2Stubs = new OAuth2Stubs(wireMockServer, rsaKeyGenerator);
+    oAuth2Stubs.stubForConfiguration();
+    oAuth2Stubs.stubForJWKS();
+
+    applicationContext.getBeanFactory().registerSingleton("wireMockServer", wireMockServer);
+    applicationContext.getBeanFactory().registerSingleton("oAuth2Stubs", oAuth2Stubs);
+    applicationContext.getBeanFactory().registerSingleton("rsaKeyGenerator", rsaKeyGenerator);
+
+    applicationContext.addApplicationListener(applicationEvent -> {
+      if (applicationEvent instanceof ContextClosedEvent) {
+        LOG.info("Stopping the WireMockServer");
+        wireMockServer.stop();
+      }
+    });
+
+    TestPropertyValues
+      .of("spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:" + wireMockServer.port() + "/auth/realms/spring")
+      .applyTo(applicationContext);
+  }
+}
