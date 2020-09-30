e4299bdc24cd1c6503cfdccb9da381a46a1283af
rieckpil <mail@philipriecks.de>
2020-09-30 18:42:29 +0200

    configure Keycloak URL conditionally

--- src/frontend/src/KeycloakService.ts
+++ src/frontend/src/KeycloakService.ts
@@ -2,7 +2,12 @@ import Keycloak from "keycloak-js";
 import {Store} from "redux";
 import {login} from "./actions";
 
-export const keycloak = Keycloak('/keycloak.json');
+export const keycloak = Keycloak({
+    "realm": "spring",
+    "url": window.location.href.includes("localhost") ? "http://localhost:8888/auth" : "http://172.17.0.1:8888/auth",
+    "clientId": "react-client"
+  }
+);
 
 export const initKeycloak = (onInitCallback: Function, store: Store) => {
   keycloak.init({
