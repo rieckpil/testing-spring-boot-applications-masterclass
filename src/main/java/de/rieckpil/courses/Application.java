d397e0ef73728143a36a4c59729a5549489299ee
rieckpil <mail@philipriecks.de>
2020-10-31 10:36:59 +0100

    disable instance data auto-configuration to resolve error log on startup

--- src/main/java/de/rieckpil/courses/Application.java
+++ src/main/java/de/rieckpil/courses/Application.java
@@ -5,9 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ContextInstanceDataAutoConfiguration.class)
 public class Application {
 
   private static final Logger LOG = LoggerFactory.getLogger(Application.class);
