037e180a5114ba7665d71754efb4fb4e4afa5af4
Philip Riecks <mail@philipriecks.de>
2021-06-02 18:58:22 +0200

    log Docker Compose container logs with Slf4j

--- src/test/java/de/rieckpil/courses/AbstractWebTest.java
+++ src/test/java/de/rieckpil/courses/AbstractWebTest.java
@@ -8,12 +8,15 @@ import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import com.codeborne.selenide.junit5.ScreenShooterExtension;
 import de.rieckpil.courses.initializer.WireMockInitializer;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -26,13 +29,17 @@ import java.time.Duration;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public abstract class AbstractWebTest {
 
+  private static Logger LOG = LoggerFactory.getLogger(AbstractWebTest.class);
+
   public static DockerComposeContainer<?> environment =
     new DockerComposeContainer<>(new File("docker-compose.yml"))
       .withExposedService("database_1", 5432, Wait.forListeningPort())
       .withExposedService("keycloak_1", 8080, Wait.forHttp("/auth").forStatusCode(200)
         .withStartupTimeout(Duration.ofSeconds(30)))
       .withExposedService("sqs_1", 9324, Wait.forListeningPort())
-      .withLocalCompose(true);
+      .withLogConsumer("keycloak_1", new Slf4jLogConsumer(LOG))
+      .withLogConsumer("database_1", new Slf4jLogConsumer(LOG))
+      .withLogConsumer("sqs_1", new Slf4jLogConsumer(LOG));
 
   @RegisterExtension
   static ScreenShooterExtension screenShooterExtension = new ScreenShooterExtension()
