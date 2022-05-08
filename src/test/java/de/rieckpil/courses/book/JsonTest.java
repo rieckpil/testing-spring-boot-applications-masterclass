adf3e2f1d6fc7c575d8ec3bce87b15fcbe47adef
Branislav Beno <brano.beno@gmail.com>
2022-05-08 18:09:07 +0200

    Suggest chapter 5 related changes

--- src/test/java/de/rieckpil/courses/book/JsonTest.java
+++ src/test/java/de/rieckpil/courses/book/JsonTest.java
@@ -6,18 +6,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
-public class JsonTest {
+class JsonTest {
 
   @Test
   void testWithJSONAssert() throws JSONException {
-    String result = "{\"name\": \"duke\", \"age\":\"42\", \"hobbies\": [\"soccer\", \"java\"]}";
-    JSONAssert.assertEquals("{\"name\": \"duke\"}", result, false);
-    JSONAssert.assertEquals("{\"hobbies\": [\"java\", \"soccer\"]}", result, false);
+    String result = """
+      {"name": "duke",
+      "age":"42",
+      "hobbies": ["soccer", "java"]}
+      """;
+    JSONAssert.assertEquals("""
+        {"name": "duke"}
+        """,
+      result, false);
+    JSONAssert.assertEquals("""
+        {"hobbies": ["java", "soccer"]}
+        """,
+      result, false);
   }
 
   @Test
   void testWithJsonPath() {
-    String result = "{\"age\":\"42\", \"name\": \"duke\", \"tags\":[\"java\", \"jdk\"], \"orders\": [42, 42, 16]}";
+    String result = """
+      {"age":"42",
+      "name": "duke",
+      "tags":["java", "jdk"],
+      "orders": [42, 42, 16]}
+      """;
 
     Assertions.assertEquals(2, JsonPath.parse(result).read("$.tags.length()", Long.class));
     Assertions.assertEquals("duke", JsonPath.parse(result).read("$.name", String.class));
