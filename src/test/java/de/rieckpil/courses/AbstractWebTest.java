70b40dd2c02c59e1b6c6cecadcd62de7d204c1c3
Branislav Beno <brano.beno@gmail.com>
2022-05-22 16:36:07 +0200

    Use try-with-resources for autocloseable instance within end-to-end tests

--- src/test/java/de/rieckpil/courses/AbstractWebTest.java
+++ src/test/java/de/rieckpil/courses/AbstractWebTest.java
@@ -1,8 +1,5 @@
 package de.rieckpil.courses;
 
-import java.io.File;
-import java.time.Duration;
-
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -23,6 +20,9 @@ import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.File;
+import java.time.Duration;
+
 @ActiveProfiles("web-test")
 @Testcontainers(disabledWithoutDocker = true)
 @ContextConfiguration(initializers = WireMockInitializer.class)
@@ -31,16 +31,20 @@ public abstract class AbstractWebTest {
 
   protected static Logger LOG = LoggerFactory.getLogger(AbstractWebTest.class);
 
-  public static DockerComposeContainer<?> environment =
-    new DockerComposeContainer<>(new File("docker-compose.yml"))
-      .withExposedService("database_1", 5432, Wait.forListeningPort())
-      .withExposedService("keycloak_1", 8080, Wait.forHttp("/auth").forStatusCode(200)
-        .withStartupTimeout(Duration.ofSeconds(45)))
-      .withExposedService("sqs_1", 9324, Wait.forListeningPort())
-      .withLogConsumer("keycloak_1", new Slf4jLogConsumer(LOG))
-      .withLogConsumer("database_1", new Slf4jLogConsumer(LOG))
-      .withLogConsumer("sqs_1", new Slf4jLogConsumer(LOG))
-      .withLocalCompose(true);
+  private static final DockerComposeContainer<?> environment = populateEnvironment();
+
+  private static DockerComposeContainer<?> populateEnvironment() {
+    try (DockerComposeContainer<?> dcc = new DockerComposeContainer<>(new File("docker-compose.yml"))) {
+      return dcc.withExposedService("database_1", 5432, Wait.forListeningPort())
+        .withExposedService("keycloak_1", 8080, Wait.forHttp("/auth").forStatusCode(200)
+          .withStartupTimeout(Duration.ofSeconds(45)))
+        .withExposedService("sqs_1", 9324, Wait.forListeningPort())
+        .withLogConsumer("keycloak_1", new Slf4jLogConsumer(LOG))
+        .withLogConsumer("database_1", new Slf4jLogConsumer(LOG))
+        .withLogConsumer("sqs_1", new Slf4jLogConsumer(LOG))
+        .withLocalCompose(true);
+    }
+  }
 
   @RegisterExtension
   static ScreenShooterExtension screenShooterExtension = new ScreenShooterExtension()
