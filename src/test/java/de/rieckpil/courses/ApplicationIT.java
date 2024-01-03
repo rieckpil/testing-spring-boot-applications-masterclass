643a06e68e2003835ce4f05d19ea1de56771e840
Philip Riecks <mail@philipriecks.de>
2024-01-03 13:49:43 +0100

    update Google formatter

--- src/test/java/de/rieckpil/courses/ApplicationIT.java
+++ src/test/java/de/rieckpil/courses/ApplicationIT.java
@@ -44,6 +44,7 @@ class ApplicationIT {
   static LocalStackContainer localStack =
       new LocalStackContainer(DockerImageName.parse("localstack/localstack:2.2.0"))
           .withServices(SQS);
+
   // can be removed with version 0.12.17 as LocalStack now has multi-region support
   // https://docs.localstack.cloud/localstack/configuration/#deprecated
   // .withEnv("DEFAULT_REGION", "eu-central-1")
